a6885e91320b78a080090d8ed6f7ae881fa70f23
Elvis Ibarra <ielvisd@gmail.com>
2018-11-02 13:22:32 -0700

    Updates

---
+++ precache-manifest.816fefe13d2f93799f1dd6c75123be70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be76ab8444faa79d3e",
+    "url": "/lambda-notes/static/js/runtime~main.37be76ab.js"
+  },
+  {
+    "revision": "9f6b7f3e6642e10a778f",
+    "url": "/lambda-notes/static/js/main.9f6b7f3e.chunk.js"
+  },
+  {
+    "revision": "7af52ac8060bd1d34457",
+    "url": "/lambda-notes/static/js/1.7af52ac8.chunk.js"
+  },
+  {
+    "revision": "9f6b7f3e6642e10a778f",
+    "url": "/lambda-notes/static/css/main.06b29d73.chunk.css"
+  },
+  {
+    "revision": "df8fa9d318f0c308dc0fc407ccac6b59",
+    "url": "/lambda-notes/index.html"
+  }
+];
